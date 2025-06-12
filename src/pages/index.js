cea1028ba6fb2c56bdab1a08fa88c46219ceece4
Daniel Shown <daniel@kungfuchicken.com>
2025-06-12 13:31:25 -0500

    hacking the front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,6 +40,16 @@ export default function Home() {
     >
       <HomepageHeader />
       <main>
+        <div class="content-container">
+          <img
+            src="img/20240909_223329667.jpeg"
+            alt="OSS developers at work"
+            class="float-left-image"
+          />
+          <div class="text-content">
+            Open Source with SLU is Saint Louis University's open source program office, initially funded by the Alfred P. Sloan Foundation, that provides students with practical software development experience on real-world projects, delivers workforce-ready graduates with industry-standard skills, and advances open science by helping researchers navigate open source software practices to secure funding and increase research impact. Through experiential learning programs, free consulting services, and advocacy initiatives, we connect student developers with faculty and external clients to build custom software solutions while championing open source practices across the university and broader community.
+          </div>
+        </div>
         <HomepageFeatures />
       </main>
     </Layout>
