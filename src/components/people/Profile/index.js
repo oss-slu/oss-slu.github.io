abb97623fd9195b1eefcfb8f09d812e70161aebf
Abhilash Kotha <abhilash.kotha@slu.edu>
2025-06-12 00:03:28 -0500

    stylistic changes

--- src/components/people/Profile/index.js
+++ src/components/people/Profile/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
-function Profile({ imgSrc, name, profileLink, description }) {
+function Profile({ imgSrc, name, profileLink,bio, description }) {
   const githubUsername = profileLink.split('/').pop();
   const githubImgSrc = `https://github.com/${githubUsername}.png`;
 
   return (
-    <div className="techLead">
+    <>
+    {bio ? 
+    <div className="lead-container">
+      <div className="lead-image">
+        <a href={profileLink}>
+          <img src={imgSrc || githubImgSrc} alt={name} />
+        </a>
+      </div>
+      <div className="lead-description">
+        <a href={profileLink}>
+          <h4>{name}</h4>
+        </a>
+        {bio && <p>{bio}</p>}
+      </div>
+    </div>
+     : 
+     <div className="techLead">
       <a href={profileLink}>
         <img src={imgSrc || githubImgSrc} alt={name} className="teamLeadImage"/>
         <p>{name}</p>
       </a>
     </div>
+     }
+    </>
   );
+  
 }
 
 export default Profile;
