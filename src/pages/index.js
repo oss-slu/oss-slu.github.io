089ba0a92e7a77b7402306d7f2acc9985ec7a13b
Abhilash Kotha <abhilash.kotha@slu.edu>
2025-06-13 16:48:56 -0500

    Image and text on home page made responsive.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import styles from "./index.module.css";
 
@@ -34,22 +36,30 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-    >
+    <Layout title={`${siteConfig.title}`}>
       <HomepageHeader />
       <main>
-        <div class="content-container">
-          <img
-            src="img/20240909_223329667.jpeg"
-            alt="OSS developers at work"
-            class="float-left-image"
-          />
-          <div class="text-content">
-            Open Source with SLU is Saint Louis University's open source program office, initially funded by the Alfred P. Sloan Foundation, that provides students with practical software development experience on real-world projects, delivers workforce-ready graduates with industry-standard skills, and advances open science by helping researchers navigate open source software practices to secure funding and increase research impact. Through experiential learning programs, free consulting services, and advocacy initiatives, we connect student developers with faculty and external clients to build custom software solutions while championing open source practices across the university and broader community.
+        <div className={styles.homeSectionContainer}>
+          <div className={styles.homeSectionText}>
+            <h2>What is Open Source with SLU?</h2>
+            <p>
+              Open Source with SLU is Saint Louis University's open source program office, initially funded by the Alfred P. Sloan Foundation, that provides students with practical software development experience on real-world projects, delivers workforce-ready graduates with industry-standard skills, and advances open science by helping researchers navigate open source software practices to secure funding and increase research impact. Through experiential learning programs, free consulting services, and advocacy initiatives, we connect student developers with faculty and external clients to build custom software solutions while championing open source practices across the university and broader community.
+            </p>
+          </div>
+          <div className={styles.homeSectionCarousel}>
+            <Carousel showThumbs={false} infiniteLoop autoPlay>
+              <div>
+                <img src="img/20240909_223329667.jpeg" alt="OSS developers at work" />
+              </div>
+              {/* <div> Additional image
+                <img src="img/something/.jpeg" alt="OSS developers at work" />
+              </div> */}
+            </Carousel>
           </div>
         </div>
+
         <HomepageFeatures />
       </main>
     </Layout>
