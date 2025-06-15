fd55f1c658ca2dd593a806d9ab36c634935eeb52
Abhilash Kotha <abhilash.kotha@slu.edu>
2025-06-15 08:06:35 -0500

    urgent fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/connect_with/solutions"
+            to="/projects/portfolio"
           >
             Check out our projects
           </Link>
